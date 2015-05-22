2bc7eb3329b9c88387b8bc686639bd206fbc46ee
Christoph Hermann <christoph.hermann@kaba.com>
2015-05-22 15:55:14 +0200

    remove destracting ribbon

--- bundle.js
+++ bundle.js
@@ -21565,16 +21565,6 @@ module.exports = Loader;
 var $__0=    require('react'),createClass=$__0.createClass;
 var Loader = require('./loader');
 
-var GithubRibbon = createClass({
-  render: function() {
-    var style = {
-      zIndex: 10000,
-      position: "absolute",
-      top: 0, right: 0, border: 0
-    };
-    return React.createElement("a", {className: "hide-on-small-only", href: "https://github.com/stoeffel/radioactive-react-example"}, React.createElement("img", {style: style, src: "https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67", alt: "Fork me on GitHub", "data-canonical-src": "https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"}))
-  }
-});
 var SearchBar = createClass({
   getInitialState: function() {
     return {
@@ -21601,7 +21591,6 @@ var SearchBar = createClass({
       React.createElement("nav", null, 
         React.createElement("div", {className: "nav-wrapper"}, 
           React.createElement(Loader, {show: this.props.showLoader}), 
-          React.createElement(GithubRibbon, null), 
           React.createElement("a", {href: "#", className: "hide-on-small-only brand-logo center"}, "Radioactive - React"), 
           React.createElement("form", {onSubmit: this.onSearch}, 
             React.createElement("div", {className: "input-field"}, 
--- src/searchBar.jsx
+++ src/searchBar.jsx
@@ -1,16 +1,6 @@
 var { createClass } = require('react');
 var Loader = require('./loader');
 
-var GithubRibbon = createClass({
-  render: function() {
-    var style = {
-      zIndex: 10000,
-      position: "absolute",
-      top: 0, right: 0, border: 0
-    };
-    return <a className="hide-on-small-only" href="https://github.com/stoeffel/radioactive-react-example"><img style={style} src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" /></a>
-  }
-});
 var SearchBar = createClass({
   getInitialState: function() {
     return {
@@ -37,7 +27,6 @@ var SearchBar = createClass({
       <nav>
         <div className="nav-wrapper">
           <Loader show={this.props.showLoader} />
-          <GithubRibbon />
           <a href="#" className="hide-on-small-only brand-logo center">Radioactive - React</a>
           <form onSubmit={this.onSearch}>
             <div className="input-field">
